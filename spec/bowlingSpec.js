f6d50a6d1ee9f3f33f2ae903e7a4411e8fb35cc8
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-05-27 13:53:12 +0100

    Add #current_roll

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -4,16 +4,30 @@ describe("Bowling", function() {
     game = new Bowling;
   });
 
-  it('should knock over a number of pins', function() {
-    game.roll(5);
-    expect(game.pins).toEqual(5)
-  });
+  describe("#roll", function() {
 
-  it('should throw an error if attempting to knock over too many pins', function() {
-    expect(function() { game.roll(11) } ).toThrow("Error - you cannot knock over more pins than there are standing!")
-  });
+    it('should knock over a number of pins', function() {
+      game.roll(5);
+      expect(game.pins).toEqual(5);
+    });
 
+    it('should throw error if attempting to knock over too many pins on first roll or a frame', function() {
+      expect(function() { game.roll(11) } ).toThrow("Error - you cannot knock over that many pins!");
+    });
 
+    it('should throw error if attempting to knock over too many pins on second roll of a frame', function() {
+      game.roll(7);
+      expect(function() { game.roll(4) } ).toThrow("Error - you cannot knock over that many pins!");
+    });
 
+    it('should set #current_roll to 1 when starting a new frame', function() {
+      expect(game.current_roll).toEqual(1);
+    });
 
+    it('should change #current_roll to 2', function() {
+      game.roll(9);
+      expect(game.current_roll).toEqual(2);
+    });
+
+    });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,11 +1,38 @@
 function Bowling() {
   this.pins = 10;
+  this.current_roll = 1;
 };
 
 Bowling.prototype.roll = function(num) {
   if(this.pins - num < 0 || num > 10) {
-    throw("Error - you cannot knock over more pins than there are standing!")
+    throw("Error - you cannot knock over that many pins!")
   } else {
   this.pins -= num
+  this.current_roll = 2;
   };
 };
+
+
+
+
+// +    it('Should change the current roll to 2', function() {
+// +      bowlingGame.roll(5);
+// +      expect(bowlingGame.current_roll).toEqual(2)
+// +    });
+// });
+// });
+//
+//
+// function Bowling() {
+//   this.pins = 10
+// +  this.current_roll = 1
+// }
+//
+// Bowling.prototype.roll = function(num) {
+//   if(this.pins - num < 0 || num > 10) {
+//     throw("You cannot knock over more pins than there are standing")
+//   } else {
+//   this.pins -= num;
+// +  this.current_roll = 2
+//   };
+// };
